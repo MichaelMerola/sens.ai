298c8afe8a6553f8000dc7371dbbbeb986e38efc
Krish Dholakiya <krishna.dholakiya@gmail.com>
2017-09-10 05:13:53 -0400

    hours

--- src/SyllabusThumb.js
+++ src/SyllabusThumb.js
@@ -18,6 +18,7 @@ class SyllabusThumb extends Component {
       <Link className={`SyllabusThumb ${this.props.underline ? 'underline' : ''}`} to={`/syllabus/${btoa(this.props.name)}`}>
         <h3>{this.props.name}</h3>
         <p className="grey">{this.props.units.length} units</p>
+        <p className="grey">~{this.props.units.length*0.5} hours</p>
         <ProgressBar units={this.props.units} />
       </Link>
     );
